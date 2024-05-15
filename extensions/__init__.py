c535e546f942e0b90e304b723a16473ca91d9f54
fujiapp <8_carpboy_74@au.com>
2024-05-15 20:25:40 +0900

    データベース初期化方法変更

--- extensions/__init__.py
+++ extensions/__init__.py
@@ -1,21 +1,4 @@
-# extensions/__init__.py
 
-from .db import db, jwt
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
-from sqlalchemy import event
-from sqlalchemy.engine import Engine
-
-
-# SQLAlchemyのインスタンスを作成
-db = SQLAlchemy()
-
-# Marshmallowのインスタンスを作成
-ma = Marshmallow()
-
-
-@event.listens_for(Engine, "connect")
-def set_sqlite_pragma(dbapi_connection, connection_record):
-    cursor = dbapi_connection.cursor()
-    cursor.execute("PRAGMA foreign_keys = ON")
-    cursor.close()
+from .db import db
+from .auth import jwt
+from .ma import ma
--- extensions/db.py
+++ extensions/db.py
@@ -1,5 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_jwt_extended import JWTManager
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
 db = SQLAlchemy()
 
-jwt = JWTManager()
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys = ON")
+    cursor.close()
