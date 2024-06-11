98bdba70d7447595de2a518151a7d65ff02c8ff9
fujiapp <8_carpboy_74@au.com>
2024-06-11 17:08:32 +0900

    authのビジネスロジック取り出し

--- api/v1/routes/auth.py
+++ api/v1/routes/auth.py
@@ -1,79 +1,36 @@
-from extensions import db
-
-
-from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, get_jwt_identity,
-                                get_jwt, set_access_cookies, set_refresh_cookies, unset_jwt_cookies,
-                                )
+from flask_jwt_extended import jwt_required
 from flask import Blueprint, jsonify, request
-from datetime import datetime, timezone
-from ..models import User, UserSchema, TokenBlocklist
-# from ..schemas import user_schema, users_schema
-from ..validators import post_user_schema, login_user_schema
 from api.error import InvalidAPIUsage
-
+from ..service.auth import signup_user, login_user, refresh_token, logout_user
 
 auth_bp = Blueprint("auth", __name__)
 
 
 @auth_bp.route('/signup', methods=['POST'])
-def signin():
+def signup():
     input_data = request.json
-    errors = post_user_schema.validate(input_data)
-    if errors:
-        raise InvalidAPIUsage(errors)
-
-    user = User.query.filter_by(username=input_data["username"]).first()
-    if user:
+    res = signup_user(input_data)
+    if not res:
         raise InvalidAPIUsage("User already exists")
-    new_user = User(**input_data)
-    db.session.add(new_user)
-    db.session.commit()
-
-    res = UserSchema().dump(new_user)
     return res, 201
 
 
 @auth_bp.route('/login', methods=['POST'])
 def login():
     data = request.json
-    errors = login_user_schema.validate(data)
-    if errors:
-        raise InvalidAPIUsage(errors)
-    auth_user = User.query.filter_by(username=data['username']).first()
-
-    if auth_user is not None and auth_user.check_password(data['password']):
-        access_token = create_access_token(identity=auth_user.user_id)
-        refresh_token = create_refresh_token(identity=auth_user.user_id)
-        resp = jsonify({'login': True})
-        set_access_cookies(resp, access_token)
-        set_refresh_cookies(resp, refresh_token)
-        return resp, 200
-    else:
-        raise InvalidAPIUsage('Invalid User.', 401)
+    res = login_user(data)
+    return res, 200
 
 
 @auth_bp.route("/refresh", methods=["POST"])
 @jwt_required(refresh=True)
 def refresh():
-    identity = get_jwt_identity()
-    access_token = create_access_token(identity=identity)
-    resp = jsonify({'refresh': True})
-    resp.set_cookie('access_token_cookie', '', expires=datetime(1970, 1, 1))
-    resp.set_cookie('access_token_cookie', access_token,
-                    samesite='None', secure=True)
+    resp,  = refresh_token()
     return resp, 200
 
 
 @auth_bp.route("/logout", methods=["DELETE"])
 @jwt_required(verify_type=False)
 def modify_token():
-    token = get_jwt()
-    jti = token["jti"]
-    ttype = token["type"]
-    now = datetime.now(timezone.utc)
-    new_block_token = TokenBlocklist(jti=jti, type=ttype, created_at=now)
-    db.session.add(new_block_token)
-    db.session.commit()
-    resp = jsonify({'logout': True})
-    unset_jwt_cookies(resp)
+    resp = logout_user()
     return resp, 200
---
+++ api/v1/service/auth.py
@@ -0,0 +1,58 @@
+from extensions import db
+from flask_jwt_extended import (create_access_token, create_refresh_token, get_jwt_identity, get_jwt,
+                                set_access_cookies, set_refresh_cookies, unset_jwt_cookies)
+from flask import jsonify
+from datetime import datetime, timezone
+from ..models import User, UserSchema, TokenBlocklist
+from ..validators import post_user_schema, login_user_schema
+from api.error import InvalidAPIUsage
+from .utils import validate_data, get_instance_by_id, save_to_db, delete_from_db
+
+
+def signup_user(data):
+
+    validate_data(post_user_schema, data)
+    user = User.query.filter_by(username=data["username"], facility_id=data["facility_id"]).first()
+    if user:
+        return None
+    new_user = User(**data)
+    save_to_db(new_user)
+    res = UserSchema().dump(new_user)
+    return res
+
+
+def login_user(data):
+    validate_data(login_user_schema, data)
+    auth_user = User.query.filter_by(username=data['username']).first()
+
+    if auth_user is not None and auth_user.check_password(data['password']):
+        access_token = create_access_token(identity=auth_user.user_id)
+        refresh_token = create_refresh_token(identity=auth_user.user_id)
+        resp = jsonify({'login': True})
+        set_access_cookies(resp, access_token)
+        set_refresh_cookies(resp, refresh_token)
+        return resp
+    else:
+        raise InvalidAPIUsage('Invalid User.', 401)
+
+
+def refresh_token():
+    identity = get_jwt_identity()
+    access_token = create_access_token(identity=identity)
+    resp = jsonify({'refresh': True})
+    resp.set_cookie('access_token_cookie', '', expires=datetime(1970, 1, 1))
+    resp.set_cookie('access_token_cookie', access_token,
+                    samesite='None', secure=True)
+    return resp
+
+
+def logout_user():
+    token = get_jwt()
+    jti = token["jti"]
+    ttype = token["type"]
+    now = datetime.now(timezone.utc)
+    new_block_token = TokenBlocklist(jti=jti, type=ttype, created_at=now)
+    save_to_db(new_block_token)
+    resp = jsonify({'logout': True})
+    unset_jwt_cookies(resp)
+    return resp
