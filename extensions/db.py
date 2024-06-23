b2b71515961fa5e811bf36716d4b81d79b98c488
fujiapp <8_carpboy_74@au.com>
2024-06-23 23:45:28 +0900

    declarative_baseのimport場所変更

--- extensions/db.py
+++ extensions/db.py
@@ -2,7 +2,7 @@ import os
 from flask_marshmallow import Marshmallow, fields
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 
 ma = Marshmallow()
