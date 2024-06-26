bde83e7eba74cb211da09db45caaf7430b3e9cdd
fujiapp <8_carpboy_74@au.com>
2024-06-26 11:51:01 +0900

    facility属性のバリデーション変更

--- api/v1/validators/__init__.py
+++ api/v1/validators/__init__.py
@@ -1,4 +1,4 @@
 from .user import post_user_schema, login_user_schema
-from .facilities import post_facility_schema
+from .facilities import post_facility_schema, post_facility_atrubute_schema
 from .employees import post_employee_schema, put_employee_schema
 from .day_off_requests import post_day_off_request_schema
--- api/v1/validators/facilities.py
+++ api/v1/validators/facilities.py
@@ -6,4 +6,9 @@ class PostFacilitySchema(Schema):
     name = fields.Str(required=True, validate=validate.Length(min=2, max=50))
 
 
+class PostFcilityAtributeSchema(Schema):
+    id = fields.Int(required=True)
+
+
 post_facility_schema = PostFacilitySchema()
+post_facility_atrubute_schema = PostFcilityAtributeSchema()
