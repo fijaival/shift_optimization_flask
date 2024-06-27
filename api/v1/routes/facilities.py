b76fbe6e004cdb3b203e62edc9bb585b1d71cb08
fujiapp <8_carpboy_74@au.com>
2024-06-27 10:00:41 +0900

    施設情報の更新まとめた

--- api/v1/routes/facilities.py
+++ api/v1/routes/facilities.py
@@ -1,9 +1,14 @@
 from flask import Blueprint, jsonify, request
 from api.error import InvalidAPIUsage
 from extensions import Base, jwt_required, self_facility_required
-from ..service.facilities import validate_and_create_facility_service, delete_facility_service, get_facility_service, add_qualification_to_facility_service, delete_qualification_from_facility_service, add_constraint_to_facility_service, delete_constraint_from_facility_service, add_task_to_facility_service, delete_task_from_facility_service
+from ..service.facilities import validate_and_create_facility_service, delete_facility_service, get_facility_service, update_facility_service
 
 facilities_bp = Blueprint('facilities', __name__)
+
+
+@facilities_bp.route('/', methods=['GET'])
+def get_all_facilities():
+    return jsonify({"message": "Hello, World!"}), 200
 
 
 @facilities_bp.route('/', methods=['POST'])
@@ -14,81 +19,29 @@ def add_facility():
     return res, 201
 
 
-@facilities_bp.route('/<int:facility_id>', methods=['DELETE'])
-@jwt_required()
-def delete_facility(facility_id):
-    """Delete a facility from the database.userより上の権限必要?"""
-    facility = delete_facility_service(facility_id)
-    if not facility:
-        raise InvalidAPIUsage("Facility not found", 404)
-    return jsonify({"message": "Facility deleted successfully!"}), 200
-
-
 @facilities_bp.route('/<int:facility_id>', methods=['GET'])
 @self_facility_required
 def get_facility(facility_id):
-    """Get a facility by its ID."""
+    """ここだけ権限異なる"""
     facility = get_facility_service(facility_id)
     if not facility:
         return InvalidAPIUsage("Facility not found", 404)
     return facility, 200
 
 
-@facilities_bp.route('/<int:facility_id>/qualifications', methods=['POST'])
-@self_facility_required
-def add_qualification_to_facility(facility_id):
-    """Add a qualification to a facility."""
+@facilities_bp.route('/<int:facility_id>', methods=['DELETE'])
+@jwt_required()
+def delete_facility(facility_id):
+    facility = delete_facility_service(facility_id)
+    if not facility:
+        raise InvalidAPIUsage("Facility not found", 404)
+    return jsonify({"message": "Facility deleted successfully!"}), 200
+
+
+@facilities_bp.route('/<int:facility_id>', methods=['PUT'])
+@jwt_required()
+def update_facility(facility_id):
+    "update employee information"
     data = request.json
-    res = add_qualification_to_facility_service(facility_id, data)
-    if not res:
-        raise InvalidAPIUsage("Qualification not found", 404)
+    res = update_facility_service(facility_id, data)
     return res, 201
-
-
-@facilities_bp.route('/<int:facility_id>/qualifications/<int:qualification_id>', methods=['DELETE'])
-@self_facility_required
-def delete_qualification_from_facility(facility_id, qualification_id):
-    res = delete_qualification_from_facility_service(facility_id, qualification_id)
-    if not res:
-        raise InvalidAPIUsage("Qualification not found", 404)
-    return res, 200
-
-
-@facilities_bp.route('/<int:facility_id>/constraints', methods=['POST'])
-@self_facility_required
-def add_constraint_to_facility(facility_id):
-    """Add a constraints to a facility."""
-    data = request.json
-    res = add_constraint_to_facility_service(facility_id, data)
-    if not res:
-        raise InvalidAPIUsage("Constraint not found", 404)
-    return res, 201
-
-
-@facilities_bp.route('/<int:facility_id>/constraints/<int:constraint_id>', methods=['DELETE'])
-@self_facility_required
-def delete_constraint_from_facility(facility_id, constraint_id):
-    res = delete_constraint_from_facility_service(facility_id, constraint_id)
-    if not res:
-        raise InvalidAPIUsage("Constraint not found", 404)
-    return res, 200
-
-
-@facilities_bp.route('/<int:facility_id>/tasks', methods=['POST'])
-@self_facility_required
-def add_task_to_facility(facility_id):
-    """Add a tasks to a facility."""
-    data = request.json
-    res = add_task_to_facility_service(facility_id, data)
-    if not res:
-        raise InvalidAPIUsage("Task not found", 404)
-    return res, 201
-
-
-@facilities_bp.route('/<int:facility_id>/tasks/<int:task_id>', methods=['DELETE'])
-@self_facility_required
-def delete_task_from_facility(facility_id, task_id):
-    res = delete_task_from_facility_service(facility_id, task_id)
-    if not res:
-        raise InvalidAPIUsage("Task not found", 404)
-    return res, 200
--- api/v1/service/facilities.py
+++ api/v1/service/facilities.py
@@ -1,6 +1,5 @@
 from flask import jsonify
-from extensions import db_session
-from ..validators import post_facility_schema, post_facility_atrubute_schema
+from ..validators import post_facility_schema, post_facility_atrubute_schema, put_facility_schema
 from ..models import Facility, FacilitySchema, Qualification, Constraint, Task
 from api.error import InvalidAPIUsage
 from .db_utils import session_scope, validate_data
@@ -32,54 +31,30 @@ def get_facility_service(facility_id):
         return FacilitySchema().dump(facility)
 
 
-def add_qualification_to_facility_service(facility_id, data):
-    return add_attribute_to_facility(facility_id, data, Qualification, 'qualifications', 'qualification_id')
-
-
-def delete_qualification_from_facility_service(facility_id, qualification_id):
-    return delete_attribute_from_facility(facility_id, qualification_id, Qualification, 'qualifications', 'qualification_id')
-
-
-def add_constraint_to_facility_service(facility_id, data):
-    return add_attribute_to_facility(facility_id, data, Constraint, 'constraints', 'constraint_id')
-
-
-def delete_constraint_from_facility_service(facility_id, constraint_id):
-    return delete_attribute_from_facility(facility_id, constraint_id, Constraint, 'constraints', 'constraint_id')
-
-
-def add_task_to_facility_service(facility_id, data):
-    return add_attribute_to_facility(facility_id, data, Task, 'tasks', 'task_id')
-
-
-def delete_task_from_facility_service(facility_id, task_id):
-    return delete_attribute_from_facility(facility_id, task_id, Task, 'tasks', 'task_id')
-
-
-def add_attribute_to_facility(facility_id, data, model_class, attribute_name, attribute_id):
+def update_facility_service(facility_id, data):
     with session_scope() as session:
-        validate_data(post_facility_atrubute_schema, data)
-        attribute = session.query(model_class).filter(getattr(model_class, attribute_id) == data['id']).first()
-        if not attribute:
-            return None
+        validate_data(put_facility_schema, data)
         facility = session.query(Facility).filter_by(facility_id=facility_id).first()
         if not facility:
             raise InvalidAPIUsage("Facility not found", 404)
-        getattr(facility, attribute_name).append(attribute)
+
+        facility.name = data["name"]
+        facility.constraints.clear()
+        facility.qualifications.clear()
+        facility.tasks.clear()
+
+        append_data_to_facility(session, facility, data, Constraint, 'constraints',
+                                'constraint_id', 'constraints', "Constraint not found")
+        append_data_to_facility(session, facility, data, Qualification, 'qualifications',
+                                'qualification_id', 'qualifications', "Qualification not found")
+        append_data_to_facility(session, facility, data, Task, 'tasks', 'task_id', 'tasks', "Task not found")
+        session.add(facility)
         return FacilitySchema().dump(facility)
 
 
-def delete_attribute_from_facility(facility_id, target_id, model_class, attribute_name, attribute_id):
-    with session_scope() as session:
-        facility = session.query(Facility).filter_by(facility_id=facility_id).first()
-        if not facility:
-            raise InvalidAPIUsage("Facility not found", 404)
-        attribute = session.query(model_class).filter(getattr(model_class, attribute_id) == target_id).first()
-        if not attribute:
-            return False
-        attribute_list = getattr(facility, attribute_name)
-        if any(getattr(a, attribute_id) == target_id for a in attribute_list):
-            attribute_list.remove(attribute)
-            return jsonify({"message": f"{model_class.__name__} deleted successfully!"})
-        else:
-            raise InvalidAPIUsage(f"The facility does not have this {model_class.__name__}", 400)
+def append_data_to_facility(session, facility, data, model, data_key, model_id_key, append_attr, not_found_message):
+    for item_data in data[data_key]:
+        found_item = session.query(model).filter_by(**{model_id_key: item_data[model_id_key]}).first()
+        if not found_item:
+            raise InvalidAPIUsage(not_found_message, 404)
+        getattr(facility, append_attr).append(found_item)
