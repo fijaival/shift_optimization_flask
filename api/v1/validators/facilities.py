b1b545009120ff170659bb4b2a4956e4a4bd07ab
fujiapp <8_carpboy_74@au.com>
2024-06-27 10:01:11 +0900

    施設のスキーマ変更

--- api/v1/validators/facilities.py
+++ api/v1/validators/facilities.py
@@ -10,5 +10,26 @@ class PostFcilityAtributeSchema(Schema):
     id = fields.Int(required=True)
 
 
+class FacilityConstraintSchema(Schema):
+    constraint_id = fields.Int(required=True)
+
+
+class FacilityQualificationSchema(Schema):
+    qualification_id = fields.Int(required=True)
+
+
+class FacilityTaskSchema(Schema):
+    task_id = fields.Int(required=True)
+
+
+class PutFacilitySchema(Schema):
+    name = fields.Str(required=True, validate=validate.Length(min=2, max=50))
+
+    constraints = fields.List(fields.Nested(FacilityConstraintSchema), required=True)
+    qualifications = fields.List(fields.Nested(FacilityQualificationSchema), required=True)
+    tasks = fields.List(fields.Nested(FacilityTaskSchema), required=True)
+
+
 post_facility_schema = PostFacilitySchema()
 post_facility_atrubute_schema = PostFcilityAtributeSchema()
+put_facility_schema = PutFacilitySchema()
